de67983653d54d2153d9f4c1d1961136274627bb
Jevgenijs Serovs <jevgenijs.serovs@gmail.com>
2021-01-13 14:26:40 +0200

    like and remove buttons

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,19 +7,37 @@ import Togglable from './Togglable'
 
 const Blog = ({ blog }) => {
 
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   // eslint-disable-next-line no-unused-vars
   const [showDetails, setShowDetails] = useState(false)
 
   const ref=React.createRef();
 
+  const likeBlog = () => {
+    console.log('I like this blog')
+  }
+
+  const deleteBlog = () => {
+    console.log('I like this blog')
+    console.log('I delete this blog')
+  }
+
   return (
-    <div id={blog.id} className='blogEntry'>
+    <div id={blog.id} style={blogStyle}>
       <b>{blog.title}</b>
       <Togglable buttonLabel='show details' ref={ref}>
         <div>
           <div>{blog.url}</div>
-          <div>likes: {blog.likes} <button>like</button></div>
+          <div>likes: {blog.likes} <button onClick={likeBlog}>like</button></div>
           <div>{blog.author}</div>
+          <div><button onClick={deleteBlog}>remove</button></div>
         </div>
         
         
